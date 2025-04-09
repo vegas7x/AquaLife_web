447953e246d8711fd22f973201d9c6dd276e9855
vegas7x <eonick0402@gmail.com>
2025-04-09 13:00:23 -0300

    Add files via upload
    primeiro upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AquaGestor from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AquaGestor />
+  </React.StrictMode>,
+)
