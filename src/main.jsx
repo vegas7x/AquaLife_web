c0d0919cd6ee6c95edbaa29d80d897cfe2c02f46
vegas7x <eonick0402@gmail.com>
2025-04-09 13:23:15 -0300

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AquaGestor from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AquaGestor />
-  </React.StrictMode>,
-)
